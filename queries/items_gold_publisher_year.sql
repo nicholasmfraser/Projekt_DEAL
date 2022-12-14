378d7f4d033238bf9cffde8be6e0acbf8481452c
Ahobert <anne@hobert.eu>
2022-12-14 15:22:15 +0100

    added figure for gold/hybrid by publisher, added captions for supplement figures

---
+++ queries/items_gold_publisher_year.sql
@@ -0,0 +1,15 @@
+SELECT
+  t1.article_pubyear, t2.publisher, t2.member, COUNT(t1.article_doi) AS n_items
+FROM 
+    projekt-deal.projekt_deal.deal_items_analysis t1
+  INNER JOIN
+    projekt-deal.projekt_deal.cr_jan21 t2
+  ON
+    t1.article_doi = t2.doi
+  INNER JOIN
+    projekt-deal.projekt_deal.upw_Feb21_08_21 t3
+  ON
+    t1.article_doi = t3.doi
+WHERE t3.oa_status ="gold"
+GROUP BY t1.article_pubyear, t2.publisher, t2.member
+ORDER BY article_pubyear, n_items desc
